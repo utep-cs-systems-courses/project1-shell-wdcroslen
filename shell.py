bb0cb05f8bb39611790f7d454ee5521f2add38d8
William Croslen <wdcroslen@miners.utep.edu>
2020-09-13 18:41:13 -0600

    add directories and cd

--- shell.py
+++ shell.py
@@ -4,10 +4,19 @@ pid = os.getpid()
 
 pr,pw = os.pipe()
 
+cd = "$ "
+curr = cd
+prev = None
+directory_list = {"$ ":["Desktop","Downloads","temp"] , "Desktop":"", "Downloads":"oof" ,"temp":"none"} #make a dir list for each dir
+
+#maybe use dict
+
+#directory_list[Desktop] gives the list of the directories in desktop
+
 def pipe_message():
 	for f in (pr,pw):
 		os.set_inheritable(f,True)
-	print("\npipe fds: pr=%d, pw=%d" % (pr,pw))
+#	print("\npipe fds: pr=%d, pw=%d" % (pr,pw))
 
 import fileinput
 
@@ -22,24 +31,81 @@ def switch_commands(argument):
 		"exit":"Now terminating...Goodbye World",
 		"Exit":"Now terminating...Goodbye World"
     } 
-    return switcher.get(argument, "Err Command Not Found") 
+    return switcher.get(argument, "command not found") 
 
+
+def update_curr_dir(dir): 
+	global cd 
+	global curr
+	global prev
+	#TODO: add cd ..
+	#TODO: updating directory is backwards
+	#TODO:  Make it sot that you can't cd into the directory you are already on.
+	if dir == "..":
+		cd = prev
+		return
+	
+	
+	if dir == curr:
+		print("-bash: cd: '%s' : No such file or directory" % dir) 
+		return
+		#might not need this 'if' or 'curr' when I use a dict
+		
+	if dir in directory_list[curr]:  #FIXME: can cd '' in desktop
+		cd = dir + " " + cd
+		curr = dir
+		
+	else: 
+		print("-bash: cd: '%s' : No such file or directory" % dir)
+	
+
+def list_directories():
+	global curr
+	if type(directory_list[curr]) == str :
+		print(directory_list[curr])
+		return
+	for i in directory_list[curr]:
+		print(i, end ="   ")
+	print("")
+	
+	
 def after_fork():
 	if rc < 0:
 		print("Fork Failed")
 		sys.exit(1)
 	elif rc == 0: #child
-		print("Hello User! Please enter a command.")
-		os.close(1)
-		os.dup(pw)
-		for fd in (pr,pw):
-			os.close(fd)
+		print("Hello User! Please enter a command.\n")
+		time.sleep(.5)
 		a = ""
 		while ( a != "exit"):
 			if a == "Exit":
 				break
-			a = input("$")
-			print(switch_commands(a))
+			time.sleep(.5)
+			a = input(cd)
+			s = switch_commands(a)
+			
+			if (a[:2] == "cd"):   #for some reason I am not getting out of bounds with "cd"
+				update_curr_dir(a[3:])   #I'm ok with that
+				
+			elif (a == "ls"):
+				list_directories()
+#				for i in directory_list:
+#					print(i, end ="   ")
+#				print("")
+				
+			else:
+				print(s + "\n")
+
+#				if (s != "command not found"):
+#					break  #break out of loop without exit
+				
+				
+				
+		os.close(1)
+		os.dup(pw)
+		for fd in (pr,pw):
+			os.close(fd)
+			
 
 	else: #parent
 		os.close(0)
@@ -50,7 +116,6 @@ def after_fork():
 			print(line)
 		
 if __name__ == "__main__":
-	print("hi")
 	pipe_message()
 	rc = os.fork()
 	after_fork()
